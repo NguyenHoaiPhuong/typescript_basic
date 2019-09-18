07d55f69288ba0c433508769a61f79efb1bb4e54
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-09-18 10:43:06 +0700

    Declare variable and some basic keywords

--- 01-Basic_Syntax/identifier.ts
+++ 01-Basic_Syntax/identifier.ts
@@ -1,22 +1,22 @@
-// Here is a comment
+// string variable
 var message:string = "Hello world";
 console.log("message:", message);
-
-var num:number = 2;
-console.log("Number:", num)
-
-num = 3.5;
-console.log("Number:", num);
-
-var var1 = null;
-console.log("var1:", var1);
-
-var1 = undefined;
-console.log("var1:", var1);
-
 var $string = "Welcome"
 console.log("$string:", $string)
 
+// number variable
+var num:number = 2;
+console.log("Number:", num)
+num = 3.5;
+console.log("Number:", num);
+
+// null and undefined varibale
+var var1 = null;
+console.log("var1:", var1);
+var1 = undefined;
+console.log("var1:", var1);
+
+// string and number
 let fullName:string = "Nguyen Hoai Phuong"
 let age:number = 34
 let sentence:string = `Hello, my name is ${fullName}.
@@ -24,5 +24,17 @@ let sentence:string = `Hello, my name is ${fullName}.
 I 'll be ${age + 1} years old next month.`;
 console.log(sentence)
 
+// type assertion
+console.log("type assertion")
+let str:any = "Hello world"
+let len:number = (<string>str).length
+console.log("string", str, "has length", len)
+
+// array of number
 let list:Array<number> = [1, 2, 3];
-console.log("list:", list)
+console.log("array:", list)
+
+// tuple
+let x:[string, number] = ["hello", 10]
+console.log("tuple:", x)
+
--- 01-Basic_Syntax/keywords.ts
+++ 01-Basic_Syntax/keywords.ts
@@ -1,10 +1,47 @@
-// forEach keyword
 let arr:Array<number> = [1, 2, 3, 4, 5, 6, 7]
+
+// forEach keyword
+console.log("iterate array by forEach keyword")
 arr.forEach(function(value, index, array) {
     console.log("arr[", index, "] = ", value)
 })
 
+// map keyword
+console.log("iterate array by map keyword")
+arr.map(function(value, index, array) {
+    console.log("arr[", index, "] = ", value)
+})
+
+// iterate array
+console.log("iterate array")
+for (let i:number = 0; i < arr.length; i++) {
+    console.log("arr[", i, "] = ", arr[i])
+}
+
+// tuple
+let tup:[string, number,string] = ["Hello", 34, "World"]
+console.log("tuple:", tup)
+
+// forEach keyword
+console.log("iterate tuple by forEach keyword")
+tup.forEach(function(value, index, array) {
+    console.log(value)
+})
+
+// map keyword
+console.log("iterate tuple by map keyword")
+tup.map(function(value, index, array) {
+    console.log(value)
+})
+
+// iterate tuple
+console.log("iterate tuple")
+for (let i:number = 0; i < tup.length; i++) {
+    console.log("tup[", i, "] = ", tup[i])
+}
+
 // if - else
+console.log("if - else keyword")
 const n:number = 3
 if (n > 0) {
     console.log("n is a positive number")
@@ -13,6 +50,7 @@ if (n > 0) {
 }
 
 // switch - case keyword
+console.log("switch - case keyword")
 switch (n) {
     case 3:
         console.log("n is equal to 3")
@@ -24,3 +62,16 @@ switch (n) {
 }
 
 // while keyword
+console.log("while keyword")
+let i:number = 10
+while (i > 0) {
+    console.log(i)
+    i -= 1
+}
+
+// enum keyword
+console.log("enum keyword")
+enum Color {Red, Green, Blue}
+let color:Color = Color.Red
+console.log(color)
+console.log(Color[1])
